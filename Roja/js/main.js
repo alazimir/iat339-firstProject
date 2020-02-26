22a0fb49c2b3dbfef9c230ea271034b7194c3771
Tyler Barlow <tyler_barlow@sfu.ca>
2020-02-26 13:17:23 -0800

    Store Filtering Updates

--- Roja/js/main.js
+++ Roja/js/main.js
@@ -1,22 +1,23 @@
 //JavaScript file
 
-var itemQuant = 0;
+//Product Page Stuff Start
+var itemCount = 0;
 
 function increaseQuantNum() {
-    itemQuant++;
-    document.getElementById('inc').innerHTML = itemQuant;
+    itemCount++;
+    document.getElementById('inc').innerHTML = itemCount;
 }
 
 function decreaseQuantNum() {
-    if (itemQuant > 0) {
-        itemQuant--;
-        document.getElementById('inc').innerHTML = itemQuant;
+    if (itemCount > 0) {
+        itemCount--;
+        document.getElementById('inc').innerHTML = itemCount;
     }
 }
 
 function addToCart() {
-    if(itemQuant > 0){
-        document.getElementById('cart').innerHTML = "cart " + itemQuant;
+    if(itemCount > 0){
+        document.getElementById('cart').innerHTML = "cart " + itemCount;
 
         //    How to change src with JS https://stackoverflow.com/questions/11722400/programmatically-change-the-src-of-an-img-tag
         document.getElementById("cartIcon").src = "images/shoppingCartIconRed.svg";
@@ -28,7 +29,7 @@ function addToCart() {
 
 //Popup Added to cart message
 function showImage() {
-    document.getElementById("added-tocart-popup").style.display = "block";
+    document.getElementById("added-tocart-popup").style.opacity = 1;
 
     hideImage();
 }
@@ -40,5 +41,129 @@ function hideImage() {
 }
 
 function doHide() {
-    document.getElementById("added-tocart-popup").style.display = "none";
+    document.getElementById("added-tocart-popup").style.opacity = 0;
 }
+
+//Product page stuff end
+
+//Store stuff start
+
+function showAll(){
+    var mild = document.getElementsByClassName("mild-heat");
+    var medium = document.getElementsByClassName("medium-heat");
+    var hot = document.getElementsByClassName("hot-heat");
+    var insane = document.getElementsByClassName("insane-heat");
+
+    for (var i = 0; i < mild.length; i++){
+        mild[i].style.display = "flex";
+    }
+    
+    for (var i = 0; i < medium.length; i++){
+        medium[i].style.display = "flex";
+    }
+    
+    for (var i = 0; i < hot.length; i++){
+        hot[i].style.display = "flex";
+    }
+    
+    for (var i = 0; i < insane.length; i++){
+        insane[i].style.display = "flex";
+    }
+}
+
+function showOnlyMild(element){
+    element.style.color = "#FF220D";
+    
+    var mild = document.getElementsByClassName("mild-heat");
+    var medium = document.getElementsByClassName("medium-heat");
+    var hot = document.getElementsByClassName("hot-heat");
+    var insane = document.getElementsByClassName("insane-heat");
+    
+
+    for (var i = 0; i < mild.length; i++){
+        mild[i].style.display = "flex";
+    }
+    
+    for (var i = 0; i < medium.length; i++){
+        medium[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < hot.length; i++){
+        hot[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < insane.length; i++){
+        insane[i].style.display = "none";
+    }
+}
+
+function showOnlyMedium(){
+    var mild = document.getElementsByClassName("mild-heat");
+    var medium = document.getElementsByClassName("medium-heat");
+    var hot = document.getElementsByClassName("hot-heat");
+    var insane = document.getElementsByClassName("insane-heat");
+
+    for (var i = 0; i < mild.length; i++){
+        mild[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < medium.length; i++){
+        medium[i].style.display = "flex";
+    }
+    
+    for (var i = 0; i < hot.length; i++){
+        hot[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < insane.length; i++){
+        insane[i].style.display = "none";
+    }
+}
+
+function showOnlyHot(){
+    var mild = document.getElementsByClassName("mild-heat");
+    var medium = document.getElementsByClassName("medium-heat");
+    var hot = document.getElementsByClassName("hot-heat");
+    var insane = document.getElementsByClassName("insane-heat");
+
+    for (var i = 0; i < mild.length; i++){
+        mild[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < medium.length; i++){
+        medium[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < hot.length; i++){
+        hot[i].style.display = "flex";
+    }
+    
+    for (var i = 0; i < insane.length; i++){
+        insane[i].style.display = "none";
+    }
+}
+
+function showOnlyInsane(){
+    var mild = document.getElementsByClassName("mild-heat");
+    var medium = document.getElementsByClassName("medium-heat");
+    var hot = document.getElementsByClassName("hot-heat");
+    var insane = document.getElementsByClassName("insane-heat");
+
+    for (var i = 0; i < mild.length; i++){
+        mild[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < medium.length; i++){
+        medium[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < hot.length; i++){
+        hot[i].style.display = "none";
+    }
+    
+    for (var i = 0; i < insane.length; i++){
+        insane[i].style.display = "flex";
+    }
+}
+//Store filtering
+
