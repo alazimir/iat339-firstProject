46baa1ea7b5371af7028bf549a9deea73ecbdf64
Tyler Barlow <tyler_barlow@sfu.ca>
2020-02-26 11:03:22 -0800

    Cart button update
    Cart icon changes from black to red when there are items in the cart.

--- Roja/js/main.js
+++ Roja/js/main.js
@@ -14,7 +14,12 @@ var i = 0;
     }
 
 function addToCart() {
+//    var cartVal = document.getElementById('cart').innerHTML;
+//    console.log(cartVal);
+    
     document.getElementById('cart').innerHTML = "cart " + i;
+    
+    document.getElementById("cartIcon").src = "images/shoppingCartIconRed.svg";
     
     window.alert("Added to cart!");
 }
