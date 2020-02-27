e280b377bd50ed348ff3a1f34521788a42a8312a
Tyler Barlow <tyler_barlow@sfu.ca>
2020-02-26 16:32:30 -0800

    Optimized the filtering JS into one function
    Also added the brand filtering

--- Roja/js/main.js
+++ Roja/js/main.js
@@ -16,7 +16,7 @@ function decreaseQuantNum() {
 }
 
 function addToCart() {
-    if(itemCount > 0){
+    if (itemCount > 0) {
         document.getElementById('cart').innerHTML = "cart " + itemCount;
 
         //    How to change src with JS https://stackoverflow.com/questions/11722400/programmatically-change-the-src-of-an-img-tag
@@ -48,122 +48,227 @@ function doHide() {
 
 //Store stuff start
 
-function showAll(){
-    var mild = document.getElementsByClassName("mild-heat");
-    var medium = document.getElementsByClassName("medium-heat");
-    var hot = document.getElementsByClassName("hot-heat");
-    var insane = document.getElementsByClassName("insane-heat");
+function showOnly(element, select) {
+    //-----Get all side buttons
+    var allFilterText = document.getElementById("sideMenu").querySelectorAll("h3");
 
-    for (var i = 0; i < mild.length; i++){
-        mild[i].style.display = "flex";
+    //-----Remove the selected class from all buttons
+    for (var i = 0; i < allFilterText.length; i++) {
+        allFilterText[i].classList.remove("selected-side-nav");
     }
-    
-    for (var i = 0; i < medium.length; i++){
-        medium[i].style.display = "flex";
-    }
-    
-    for (var i = 0; i < hot.length; i++){
-        hot[i].style.display = "flex";
-    }
-    
-    for (var i = 0; i < insane.length; i++){
-        insane[i].style.display = "flex";
-    }
-}
 
-function showOnlyMild(element){
-    element.style.color = "#FF220D";
+    //-----Set the button that called the function to selected class
+    element.classList.add("selected-side-nav");
+
+    //-----Set arrays of all cards, and each heat level.
+    //Selector info https://developer.mozilla.org/en-US/docs/Web/API/Document/querySelectorAll
     
+    //All sauces
+    var all = document.getElementsByClassName('product-listing');
+    
+    //By heat
     var mild = document.getElementsByClassName("mild-heat");
     var medium = document.getElementsByClassName("medium-heat");
     var hot = document.getElementsByClassName("hot-heat");
     var insane = document.getElementsByClassName("insane-heat");
     
+    //By brand
+    var melindas = document.getElementsByClassName("melindas-brand");
+    var daves = document.getElementsByClassName("daves-brand");
+    var currie = document.getElementsByClassName("currie-brand");
+    var tessemaes = document.getElementsByClassName("tessemaes-brand");
 
-    for (var i = 0; i < mild.length; i++){
-        mild[i].style.display = "flex";
+    //-----First set all to be hidden
+    for (var i = 0; i < all.length; i++) {
+        all[i].style.display = "none";
+    }
+
+    //-----Change all back to flex
+    if (select == 'all-heat') {
+        for (var i = 0; i < all.length; i++) {
+            all[i].style.display = "flex";
+        }
+    }
+
+    //-----Change mild classes back to flex
+    if (select == 'mild-heat') {
+        for (var i = 0; i < mild.length; i++) {
+            mild[i].style.display = "flex";
+        }
+    }
+
+    //-----Change medium classes back to flex
+    if (select == 'medium-heat') {
+        for (var i = 0; i < medium.length; i++) {
+            medium[i].style.display = "flex";
+        }
+    }
+
+    //-----Change hot classes back to flex
+    if (select == 'hot-heat') {
+        for (var i = 0; i < hot.length; i++) {
+            hot[i].style.display = "flex";
+        }
+    }
+
+    //-----Change insane classes back to flex
+    if (select == 'insane-heat') {
+        for (var i = 0; i < insane.length; i++) {
+            insane[i].style.display = "flex";
+        }
     }
     
-    for (var i = 0; i < medium.length; i++){
-        medium[i].style.display = "none";
+    //-----Change melindas back to flex
+    if (select == 'melindas-brand') {
+        for (var i = 0; i < melindas.length; i++) {
+            melindas[i].style.display = "flex";
+        }
     }
     
-    for (var i = 0; i < hot.length; i++){
-        hot[i].style.display = "none";
+    //-----Change daves back to flex
+    if (select == 'daves-brand') {
+        for (var i = 0; i < daves.length; i++) {
+            daves[i].style.display = "flex";
+        }
     }
     
-    for (var i = 0; i < insane.length; i++){
-        insane[i].style.display = "none";
+    //-----Change currie back to flex
+    if (select == 'currie-brand') {
+        for (var i = 0; i < currie.length; i++) {
+            currie[i].style.display = "flex";
+        }
+    }
+    
+    //-----Change tessemaes back to flex
+    if (select == 'tessemaes-brand') {
+        for (var i = 0; i < tessemaes.length; i++) {
+            tessemaes[i].style.display = "flex";
+        }
     }
 }
 
-function showOnlyMedium(){
-    var mild = document.getElementsByClassName("mild-heat");
-    var medium = document.getElementsByClassName("medium-heat");
-    var hot = document.getElementsByClassName("hot-heat");
-    var insane = document.getElementsByClassName("insane-heat");
-
-    for (var i = 0; i < mild.length; i++){
-        mild[i].style.display = "none";
-    }
-    
-    for (var i = 0; i < medium.length; i++){
-        medium[i].style.display = "flex";
-    }
-    
-    for (var i = 0; i < hot.length; i++){
-        hot[i].style.display = "none";
-    }
-    
-    for (var i = 0; i < insane.length; i++){
-        insane[i].style.display = "none";
-    }
-}
-
-function showOnlyHot(){
-    var mild = document.getElementsByClassName("mild-heat");
-    var medium = document.getElementsByClassName("medium-heat");
-    var hot = document.getElementsByClassName("hot-heat");
-    var insane = document.getElementsByClassName("insane-heat");
-
-    for (var i = 0; i < mild.length; i++){
-        mild[i].style.display = "none";
-    }
-    
-    for (var i = 0; i < medium.length; i++){
-        medium[i].style.display = "none";
-    }
-    
-    for (var i = 0; i < hot.length; i++){
-        hot[i].style.display = "flex";
-    }
-    
-    for (var i = 0; i < insane.length; i++){
-        insane[i].style.display = "none";
-    }
-}
-
-function showOnlyInsane(){
-    var mild = document.getElementsByClassName("mild-heat");
-    var medium = document.getElementsByClassName("medium-heat");
-    var hot = document.getElementsByClassName("hot-heat");
-    var insane = document.getElementsByClassName("insane-heat");
-
-    for (var i = 0; i < mild.length; i++){
-        mild[i].style.display = "none";
-    }
-    
-    for (var i = 0; i < medium.length; i++){
-        medium[i].style.display = "none";
-    }
-    
-    for (var i = 0; i < hot.length; i++){
-        hot[i].style.display = "none";
-    }
-    
-    for (var i = 0; i < insane.length; i++){
-        insane[i].style.display = "flex";
-    }
-}
+//Old way to do it. New way is condenced into one function
+//
+//function showAll(element) {
+//    var allFilterText = document.getElementById("sideMenu").querySelectorAll("h3");
+//
+//    for (var i = 0; i < allFilterText.length; i++) {
+//        allFilterText[i].classList.remove("selected-side-nav");
+//    }
+//
+//    element.classList.add("selected-side-nav");
+//
+//    var mild = document.getElementsByClassName("mild-heat");
+//    var medium = document.getElementsByClassName("medium-heat");
+//    var hot = document.getElementsByClassName("hot-heat");
+//    var insane = document.getElementsByClassName("insane-heat");
+//
+//    for (var i = 0; i < mild.length; i++) {
+//        mild[i].style.display = "flex";
+//    }
+//
+//    for (var i = 0; i < medium.length; i++) {
+//        medium[i].style.display = "flex";
+//    }
+//
+//    for (var i = 0; i < hot.length; i++) {
+//        hot[i].style.display = "flex";
+//    }
+//
+//    for (var i = 0; i < insane.length; i++) {
+//        insane[i].style.display = "flex";
+//    }
+//}
+//function showOnlyMedium(element) {
+//    var allFilterText = document.getElementById("sideMenu").querySelectorAll("h3");
+//
+//    for (var i = 0; i < allFilterText.length; i++) {
+//        allFilterText[i].classList.remove("selected-side-nav");
+//    }
+//
+//    element.classList.add("selected-side-nav");
+//
+//    var mild = document.getElementsByClassName("mild-heat");
+//    var medium = document.getElementsByClassName("medium-heat");
+//    var hot = document.getElementsByClassName("hot-heat");
+//    var insane = document.getElementsByClassName("insane-heat");
+//
+//    for (var i = 0; i < mild.length; i++) {
+//        mild[i].style.display = "none";
+//    }
+//
+//    for (var i = 0; i < medium.length; i++) {
+//        medium[i].style.display = "flex";
+//    }
+//
+//    for (var i = 0; i < hot.length; i++) {
+//        hot[i].style.display = "none";
+//    }
+//
+//    for (var i = 0; i < insane.length; i++) {
+//        insane[i].style.display = "none";
+//    }
+//}
+//
+//function showOnlyHot(element) {
+//    var allFilterText = document.getElementById("sideMenu").querySelectorAll("h3");
+//
+//    for (var i = 0; i < allFilterText.length; i++) {
+//        allFilterText[i].classList.remove("selected-side-nav");
+//    }
+//
+//    element.classList.add("selected-side-nav");
+//
+//    var mild = document.getElementsByClassName("mild-heat");
+//    var medium = document.getElementsByClassName("medium-heat");
+//    var hot = document.getElementsByClassName("hot-heat");
+//    var insane = document.getElementsByClassName("insane-heat");
+//
+//    for (var i = 0; i < mild.length; i++) {
+//        mild[i].style.display = "none";
+//    }
+//
+//    for (var i = 0; i < medium.length; i++) {
+//        medium[i].style.display = "none";
+//    }
+//
+//    for (var i = 0; i < hot.length; i++) {
+//        hot[i].style.display = "flex";
+//    }
+//
+//    for (var i = 0; i < insane.length; i++) {
+//        insane[i].style.display = "none";
+//    }
+//}
+//
+//function showOnlyInsane(element) {
+//    var allFilterText = document.getElementById("sideMenu").querySelectorAll("h3");
+//
+//    for (var i = 0; i < allFilterText.length; i++) {
+//        allFilterText[i].classList.remove("selected-side-nav");
+//    }
+//
+//    element.classList.add("selected-side-nav");
+//
+//    var mild = document.getElementsByClassName("mild-heat");
+//    var medium = document.getElementsByClassName("medium-heat");
+//    var hot = document.getElementsByClassName("hot-heat");
+//    var insane = document.getElementsByClassName("insane-heat");
+//
+//    for (var i = 0; i < mild.length; i++) {
+//        mild[i].style.display = "none";
+//    }
+//
+//    for (var i = 0; i < medium.length; i++) {
+//        medium[i].style.display = "none";
+//    }
+//
+//    for (var i = 0; i < hot.length; i++) {
+//        hot[i].style.display = "none";
+//    }
+//
+//    for (var i = 0; i < insane.length; i++) {
+//        insane[i].style.display = "flex";
+//    }
+//}
 //Store filtering
-
