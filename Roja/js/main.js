9f2edf31275a396627c5ef9e468e6dda350e7bc2
Tyler Barlow <tyler_barlow@sfu.ca>
2020-03-01 19:09:04 -0800

    Store has header and description
    Each filter has it's own header and description changed with the JS

--- Roja/js/main.js
+++ Roja/js/main.js
@@ -107,20 +107,40 @@ function showOnly(element, select) {
 
     //-----Change all back to flex
     if (select == 'all-heat') {
+        document.getElementById("storeCurrentHeader").innerHTML = "All Sauces";
+        document.getElementById("storeCurrentPara").innerHTML = "Every single sauce we carry. We are confident you can find something you like. If you can't, <a class=\"red-bold-text\" href=\"contact.html\">send us a message</a> with any suggestions!";
+
+            
         for (var i = 0; i < all.length; i++) {
+            all[i].style.display = "flex";
+        }
+    }
+    
+    if (select == 'sale') {
+        document.getElementById("storeCurrentHeader").innerHTML = "On Sale Now";
+        document.getElementById("storeCurrentPara").innerHTML = "These sales won't last long! Grab yourself a discounted bottle today";
+
+            
+        for (var i = 0; i < mild.length; i++) {
             all[i].style.display = "flex";
         }
     }
 
     //-----Change mild classes back to flex
     if (select == 'mild-heat') {
+        document.getElementById("storeCurrentHeader").innerHTML = "Mild Sauces";
+        document.getElementById("storeCurrentPara").innerHTML = "For those who like a lot of flavor without the heat. Try a new mild sauce like a tangy green chile sauce from Inthisorn Food's, or mild buffalo.";
+        
         for (var i = 0; i < mild.length; i++) {
             mild[i].style.display = "flex";
         }
     }
 
-    //-----Change medium classes back to flex
+      //-----Change medium classes back to flex
     if (select == 'medium-heat') {
+        document.getElementById("storeCurrentHeader").innerHTML = "Medium Sauces";
+        document.getElementById("storeCurrentPara").innerHTML = "While these sauces aren't going to burn your tounge off their flavour is nothing to scoff at. If you're craving a little kick with your food this is the place for you.";
+        
         for (var i = 0; i < medium.length; i++) {
             medium[i].style.display = "flex";
         }
@@ -128,6 +148,9 @@ function showOnly(element, select) {
 
     //-----Change hot classes back to flex
     if (select == 'hot-heat') {
+        document.getElementById("storeCurrentHeader").innerHTML = "Hot Sauces";
+        document.getElementById("storeCurrentPara").innerHTML = "Now were bringing the heat. If you're here for the name of the game, jump into the hot sauces. Theres nothing quite like the burn of a habanero sauce.";
+        
         for (var i = 0; i < hot.length; i++) {
             hot[i].style.display = "flex";
         }
@@ -135,6 +158,9 @@ function showOnly(element, select) {
 
     //-----Change insane classes back to flex
     if (select == 'insane-heat') {
+        document.getElementById("storeCurrentHeader").innerHTML = "Insane Sauces";
+        document.getElementById("storeCurrentPara").innerHTML = "Only for the most extreme hot sauce fanatics. Caution required when testing a new ultra spicy ghost pepper sauce. It won't be an experience you soon forget.";
+        
         for (var i = 0; i < insane.length; i++) {
             insane[i].style.display = "flex";
         }
@@ -142,6 +168,9 @@ function showOnly(element, select) {
 
     //-----Change melindas back to flex
     if (select == 'melindas-brand') {
+        document.getElementById("storeCurrentHeader").innerHTML = "Melindas Sauces"
+        document.getElementById("storeCurrentPara").innerHTML = "";
+            
         for (var i = 0; i < melindas.length; i++) {
             melindas[i].style.display = "flex";
         }
@@ -149,6 +178,9 @@ function showOnly(element, select) {
 
     //-----Change daves back to flex
     if (select == 'daves-brand') {
+        document.getElementById("storeCurrentHeader").innerHTML = "Dave's Sauces"
+        document.getElementById("storeCurrentPara").innerHTML = "";
+    
         for (var i = 0; i < daves.length; i++) {
             daves[i].style.display = "flex";
         }
@@ -156,6 +188,9 @@ function showOnly(element, select) {
 
     //-----Change currie back to flex
     if (select == 'currie-brand') {
+        document.getElementById("storeCurrentHeader").innerHTML = "Currie Sauces"
+        document.getElementById("storeCurrentPara").innerHTML = "";
+            
         for (var i = 0; i < currie.length; i++) {
             currie[i].style.display = "flex";
         }
@@ -163,6 +198,9 @@ function showOnly(element, select) {
 
     //-----Change tessemaes back to flex
     if (select == 'tessemaes-brand') {
+        document.getElementById("storeCurrentHeader").innerHTML = "Tessemaes Sauces"
+        document.getElementById("storeCurrentPara").innerHTML = "";
+            
         for (var i = 0; i < tessemaes.length; i++) {
             tessemaes[i].style.display = "flex";
         }
